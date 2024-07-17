15288a56ef1d2ca8e8518dc5a89c33189b23494f
Samyuktha-0301 <103641209+Samyuktha-0301@users.noreply.github.com>
2024-07-17 21:58:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+// src/App.js
+import React from 'react';
 import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import About from './utilities/About';
+import Login from './utilities/Login';
+import Signup from './utilities/Signup';
+import Navbar from './components/Navbar';
+import Home from './utilities/Home';
 
 function App() {
   return (
-    <div className="App">
-      <TodoWrapper  />
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<Signup />} />
+          {/* Other components and content */}
+        </Routes>
+      </Router>
     </div>
   );
 }
